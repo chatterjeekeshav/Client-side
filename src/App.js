78ac56f728e6dc85a36b8b9ad66672e59459440e
Keshav <keshavchatterjee72@gmail.com>
2023-02-25 13:26:23 +0530

    Code added to githhub

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import Header from './components/common/header/Header.jsx'
+import "./App.css"
+import { BrowserRouter as Router, Switch , Route, Link } from 'react-router-dom'
+import Homepages from './components/home/Homepages.jsx'
+import ImageSlider from './components/carousel/ImageSlider.jsx'
+import images from './components/carousel/images.jsx'
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Router>
+      <Header/>
+      <ImageSlider images={images}/>
+            {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/" component={Homepages} />
+        </Switch>
+    </Router>
+    </>
+  )
 }
 
-export default App;
+export default App
